c95f9d54c1bd2c21919750ad482556c0e86647e4
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-02-12 22:07:05 +0330

    filter-component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
-import React from 'react'
+import React from "react";
 import data from "./data.json";
-import Products from './components/Products'
+import Products from "./components/Products";
+import Filter from "./components/Filter";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      products : data.products,
+      products: data.products,
       size: "",
       sort: "",
-    }
+    };
   }
+  sortProducts = (e) => {
+    const sort = e.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id < b._id
+            ? 1
+            : -1
+        ),
+    }));
+  };
+  filterProducts = (e) => {
+    if (e.target.value === "") {
+      this.setState({ size: e.target.value, products: data.products });
+    } else {
+      this.setState({
+        size: e.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(e.target.value) >= 0
+        ),
+      });
+    }
+  };
   render() {
     return (
       <div className="grid-container">
@@ -20,11 +54,16 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              />
               <Products products={this.state.products} />
             </div>
-            <div className="sidebar">
-              Cart Items
-            </div>
+            <div className="sidebar">Cart Items</div>
           </div>
         </main>
         <footer>All right is reserved.</footer>
