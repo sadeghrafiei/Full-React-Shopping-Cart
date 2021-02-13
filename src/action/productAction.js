2cc459b39c9ba12949e630f9e0c51d478bfaffd0
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-02-13 16:03:32 +0330

    add-redux-filter

--- src/action/productAction.js
+++ src/action/productAction.js
@@ -1,10 +1,28 @@
-import { FETCH_PRODUCTS } from "../types"
+import {
+  FETCH_PRODUCTS,
+  FILTER_PRODUCTS_BY_SIZE,
+  ORDER_PRODUCTS_BY_PRICE,
+} from "../types";
 
 export const fetchProducts = () => async (dispatch) => {
- const res =  await fetch("/api/products");
- const data = await res.json()
- dispatch({
-     type: FETCH_PRODUCTS,
-     payload: data
- })
-} 
+  const res = await fetch("/api/products");
+  const data = await res.json();
+  dispatch({
+    type: FETCH_PRODUCTS,
+    payload: data,
+  });
+};
+
+export const filterProducts = (products, size) => (dispatch) => {
+  dispatch({
+    type: FILTER_PRODUCTS_BY_SIZE,
+    payload: {
+      size: size,
+      items:
+        size === ""
+          ? products
+          : products.filter((x) => x.availableSizes.indexOf(size) >= 0),
+    },
+  });
+};
+
